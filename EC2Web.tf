8de8be1c6a6aca25cacbfd7a4cb80c782452e8dd
tejaswini0597 <tejaswini0597@gmail.com>
2023-06-29 18:03:36 +0530

    aws provider

--- EC2Web.tf
+++ EC2Web.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    access_key="AKIAQH3PFWDTGPLXOH4F"
+    secret_key="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"
+    region="us-east-1"
+}
+
 resource "aws_instance" "Demo2" {
     ami="ami-03b8a287edc0c1253"
     instance_type="t2.micro"
